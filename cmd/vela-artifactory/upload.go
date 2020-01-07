7c3d1b487957c01fe8bafdc5fbbfaad6e01be31d
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-07 11:43:13 -0600

    feat: add upload struct for plugin

---
+++ cmd/vela-artifactory/upload.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Upload represents the plugin configuration for upload information.
+type Upload struct {
+	// file path to load arguments from
+	ArgsFile string
+	// enables pretending to upload the artifact(s)
+	DryRun bool
+	// enables uploading artifacts to exact target path (excludes source file hierarchy)
+	Flat bool
+	// enables including directories and files from sources
+	IncludeDirs bool
+	// enables uploading sub-directories for the sources
+	Recursive bool
+	// enables reading the sources as a regular expression
+	Regexp bool
+	// target path to upload artifact(s) to
+	Path string
+	// list of files to upload
+	Sources []string
+}
+
+// Validate verifies the Upload is properly configured.
+func (u *Upload) Validate() error {
+	logrus.Trace("validating upload plugin configuration")
+
+	if len(u.Path) == 0 {
+		return fmt.Errorf("no upload path provided")
+	}
+
+	if len(u.Sources) == 0 {
+		return fmt.Errorf("no upload sources provided")
+	}
+
+	return nil
+}
