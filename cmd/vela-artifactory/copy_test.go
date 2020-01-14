2b2618fd4fbfc258574e4eeaeaa5b3949fe63394
Jordan Brockopp <jordan.brockopp@target.com>
2020-01-14 10:53:39 -0600

    feat: add exec method for copy (#18)

--- cmd/vela-artifactory/copy_test.go
+++ cmd/vela-artifactory/copy_test.go
@@ -6,6 +6,34 @@ package main
 
 import "testing"
 
+func TestArtifactory_Copy_Exec_Error(t *testing.T) {
+	// setup types
+	config := &Config{
+		Action:   "copy",
+		APIKey:   "superSecretAPIKey",
+		Password: "superSecretPassword",
+		URL:      "http://localhost:8081/artifactory",
+		Username: "octocat",
+	}
+
+	cli, err := config.New()
+	if err != nil {
+		t.Errorf("Unable to create Artifactory client: %v", err)
+	}
+
+	c := &Copy{
+		Flat:      false,
+		Recursive: false,
+		Path:      "foo/bar",
+		Target:    "bar/foo",
+	}
+
+	err = c.Exec(cli)
+	if err == nil {
+		t.Errorf("Exec should have returned err")
+	}
+}
+
 func TestArtifactory_Copy_Validate(t *testing.T) {
 	// setup types
 	c := &Copy{
