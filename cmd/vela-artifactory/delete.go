2a4ff4b5892025e59223516cf9ee25810ab48481
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-14 11:21:59 -0600

    feat: add exec method for delete

--- cmd/vela-artifactory/delete.go
+++ cmd/vela-artifactory/delete.go
@@ -8,6 +8,10 @@ import (
 	"fmt"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/jfrog/jfrog-client-go/artifactory"
+	"github.com/jfrog/jfrog-client-go/artifactory/services"
+	"github.com/jfrog/jfrog-client-go/artifactory/services/utils"
 )
 
 const deleteAction = "delete"
@@ -22,6 +26,34 @@ type Delete struct {
 	Recursive bool
 	// target path to artifact(s) to remove
 	Path string
+}
+
+// Exec formats and runs the commands for removing artifacts in Artifactory.
+func (d *Delete) Exec(cli *artifactory.ArtifactoryServicesManager) error {
+	logrus.Trace("running delete with provided configuration")
+
+	// create new delete parameters
+	p := services.NewDeleteParams()
+
+	// add delete configuration to delete parameters
+	p.ArtifactoryCommonParams = &utils.ArtifactoryCommonParams{
+		Pattern:   d.Path,
+		Recursive: d.Recursive,
+	}
+
+	// send API call to capture paths to artifacts in Artifactory
+	paths, err := cli.GetPathsToDelete(p)
+	if err != nil {
+		return err
+	}
+
+	// send API call to delete artifacts in Artifactory
+	_, err = cli.DeleteFiles(paths)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Validate verifies the Delete is properly configured.
