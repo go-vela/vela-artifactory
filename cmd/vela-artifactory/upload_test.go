e58f5e55f7ee5747d1c91ae05bfbc4d54f8341de
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-01-14 19:41:03 -0600

    feat: add exec method for upload

--- cmd/vela-artifactory/upload_test.go
+++ cmd/vela-artifactory/upload_test.go
@@ -6,6 +6,38 @@ package main
 
 import "testing"
 
+func TestArtifactory_Upload_Exec_Error(t *testing.T) {
+	// setup types
+	config := &Config{
+		Action:   "upload",
+		APIKey:   "superSecretAPIKey",
+		Password: "superSecretPassword",
+		URL:      "http://localhost:8081/artifactory",
+		Username: "octocat",
+	}
+
+	cli, err := config.New()
+	if err != nil {
+		t.Errorf("Unable to create Artifactory client: %v", err)
+	}
+
+	u := &Upload{
+		ArgsFile:    "",
+		DryRun:      false,
+		Flat:        true,
+		IncludeDirs: false,
+		Recursive:   true,
+		Regexp:      false,
+		Path:        "foo/bar",
+		Sources:     []string{"baz.txt"},
+	}
+
+	err = u.Exec(cli)
+	if err == nil {
+		t.Errorf("Exec should have returned err")
+	}
+}
+
 func TestArtifactory_Upload_Validate(t *testing.T) {
 	// setup types
 	u := &Upload{
