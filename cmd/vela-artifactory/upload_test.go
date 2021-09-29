c5ef47dba348c207a27aeda40bbadde3d6692d6a
Jordan <Jordan.Brockopp@target.com>
2021-09-29 14:58:57 -0500

    chore: address linter feedback

--- cmd/vela-artifactory/upload_test.go
+++ cmd/vela-artifactory/upload_test.go
@@ -6,9 +6,10 @@ package main
 
 import (
 	"errors"
+	"testing"
+
 	"github.com/jfrog/jfrog-client-go/artifactory/services"
 	"github.com/jfrog/jfrog-client-go/artifactory/services/utils"
-	"testing"
 )
 
 func TestArtifactory_Upload_Exec_Error(t *testing.T) {
@@ -131,6 +132,7 @@ type MockArtifactoryService struct {
 	Fail bool
 }
 
+// nolint: lll // ignore long line length due to parameters
 func (a *MockArtifactoryService) UploadFiles(...services.UploadParams) ([]utils.FileInfo, int, int, error) {
 	if a.Fail {
 		return nil, 0, 1, errors.New("upload failed")
@@ -142,9 +144,11 @@ func (a *MockArtifactoryService) Copy(services.MoveCopyParams) (int, int, error)
 	return 0, 0, nil
 }
 
+// nolint: lll // ignore long line length due to parameters
 func (a *MockArtifactoryService) GetPathsToDelete(services.DeleteParams) ([]utils.ResultItem, error) {
 	return nil, nil
 }
+
 func (a *MockArtifactoryService) DeleteFiles([]utils.ResultItem) (int, error) {
 	return 0, nil
 }
